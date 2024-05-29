7ff7a58dbfd091d699b3083ff6d69e891d0b0886
prabhatsuman <mr.prabhatsuman@gmail.com>
2024-05-29 12:58:11 +0530

    Step -2 2 pointers cf edu

---
+++ A_Segment_with_Small_Sum.cpp
@@ -0,0 +1,108 @@
+/*
+    Prabhat_007
+*/
+#include <bits/stdc++.h>
+#define ll long long
+#define M 1000000007
+#define nline '\n'
+using namespace std;
+
+typedef vector<int> vi;
+#define all(v) (v).begin(), (v).end()
+typedef vector<ll> vl;
+#define read(v)       \
+    for (auto &x : v) \
+        cin >> x;
+
+#define yes cout << "YES" << nline;
+#define no cout << "NO" << nline;
+void __print(int x) { cerr << x; }
+void __print(long x) { cerr << x; }
+void __print(long long x) { cerr << x; }
+void __print(unsigned x) { cerr << x; }
+void __print(unsigned long x) { cerr << x; }
+void __print(unsigned long long x) { cerr << x; }
+void __print(float x) { cerr << x; }
+void __print(double x) { cerr << x; }
+void __print(long double x) { cerr << x; }
+void __print(char x) { cerr << '\'' << x << '\''; }
+void __print(const char *x) { cerr << '\"' << x << '\"'; }
+void __print(const string &x) { cerr << '\"' << x << '\"'; }
+void __print(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V>
+void __print(const pair<T, V> &x)
+{
+    cerr << '{';
+    __print(x.first);
+    cerr << ',';
+    __print(x.second);
+    cerr << '}';
+}
+template <typename T>
+void __print(const T &x)
+{
+    int f = 0;
+    cerr << '{';
+    for (auto &i : x)
+        cerr << (f++ ? "," : ""), __print(i);
+    cerr << "}";
+}
+void _print() { cerr << "]\n"; }
+template <typename T, typename... V>
+void _print(T t, V... v)
+{
+    __print(t);
+    if (sizeof...(v))
+        cerr << ", ";
+    _print(v...);
+}
+#ifndef ONLINE_JUDGE
+#define debug(x...)               \
+    cerr << "[" << #x << "] = ["; \
+    _print(x)
+#else
+#define debug(x...)
+#endif
+#define godspeed                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+
+/* -----------------------------Code Begins from here-------------------------------------------*/
+void solve()
+{
+    ll n,s;
+    cin>>n>>s;
+    vector<ll> a(n);
+    read(a);
+    ll left=0,right=0;
+    ll sum=0;
+    ll ans=0;
+    while(right<n)
+    {
+        sum+=a[right];
+        while(sum>s)
+        {
+            sum-=a[left];
+            left++;
+        }
+        ans=max(ans,right-left+1);
+        right++;
+
+    }
+    cout<<ans<<nline;
+
+}
+
+signed main()
+{
+    godspeed;
+    int t=1;
+    // cin >> t;
+
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
